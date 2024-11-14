3a250c19ba23101ff9b322df724b6bfea30f146b
Mauricio Servatius Hendrikx <mauricio.servatius@gmail.com>
2024-11-14 08:48:14 -0300

    main

--- project/main.py
+++ project/main.py
@@ -6,6 +6,45 @@ from sqlalchemy import and_
 from werkzeug.security import generate_password_hash
 from flask_login import login_required, current_user
 from . import db
-from .models import User, Player
+from .models import User
 
 main = Blueprint("main", __name__)
+
+@main.route("/profile")
+@login_required
+def profile():
+
+    return render_template("profile.html", current_user=current_user)
+
+
+@main.route("/profile", methods=["POST"])
+@login_required
+def profile_post():
+
+    password = request.form.get("password")
+    repass = request.form.get("repass")
+    name = request.form.get("name")
+    email = request.form.get("email")
+
+    if password != repass:
+        flash("Password está diferente")
+        flash("alert-danger")
+        return redirect(url_for("main.profile"))
+
+    if '@' not in email:
+        flash("Entrar E-mail válido")
+        flash("alert-danger")
+        return redirect(url_for("main.profile"))
+            
+    if password != "":
+        current_user.password = generate_password_hash(password, method="pbkdf2:sha256")
+
+    if name != "":
+        current_user.name = name
+        
+    current_user.email = email
+
+    db.session.add(current_user)
+    db.session.commit()
+
+    return redirect(url_for("main.profile"))
